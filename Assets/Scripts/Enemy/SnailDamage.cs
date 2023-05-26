a72509339355a5c8f3360dc033c8d0c72f15a894
nguyen-duc-viet <61795479+nguyen-duc-viet@users.noreply.github.com>
2023-05-26 07:41:19 +0700

    code logic for snail

--- Assets/Scripts/Enemy/SnailDamage.cs
+++ Assets/Scripts/Enemy/SnailDamage.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class SnailDamage : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] protected float damage;
+    private float enemyHeadCoordinateY;
+    private float playerFootCoordinateY;
+
+    private void Start()
     {
-        
+        enemyHeadCoordinateY = transform.position.y + 0.5f;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //SnailMovement.die = true;
+            playerFootCoordinateY = collision.gameObject.GetComponent<Transform>().position.y;
+            if (playerFootCoordinateY > enemyHeadCoordinateY)
+            {
+                //transform.GetComponent<SnailMovement>().die = true;
+                StartCoroutine(MoveObjectToRight(gameObject, 2.5f));
+            }
+            else
+            {
+                collision.gameObject.GetComponent<PlayerHealth>().TakeDamage(damage);
+            }
+        }
+    }
+
+    IEnumerator MoveObjectToRight(GameObject objectToMove, float speed)
+    {
+        float distanceToMove = speed * Time.deltaTime;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < 12f)
+        {
+            objectToMove.transform.Translate(Vector3.right * distanceToMove);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        objectToMove.SetActive(false);
     }
 }
