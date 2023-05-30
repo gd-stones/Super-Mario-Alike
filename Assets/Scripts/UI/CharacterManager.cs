e85eab2323e5907972110e7ed2d550df28e4526e
nguyen-duc-viet <61795479+nguyen-duc-viet@users.noreply.github.com>
2023-05-31 06:27:53 +0700

    fix logic selection character

--- Assets/Scripts/UI/CharacterManager.cs
+++ Assets/Scripts/UI/CharacterManager.cs
@@ -2,18 +2,15 @@ using UnityEngine;
 
 public class CharacterManager : MonoBehaviour
 {
-    public static string activeCharacter;
-    public static GameObject character;
+    public GameObject character;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-
+        character = GameObject.Find(SelectionCharacter.Instance.nameCharacter);
     }
 
     private void Update()
     {
-        character = GameObject.Find(activeCharacter);
         //print("------------------ " + character.name.ToString());
 
         if (character != null)
@@ -30,12 +27,45 @@ public class CharacterManager : MonoBehaviour
         }
         else
         {
-            print("sfsdfvsdfsdf");
+            print("character is null");
         }
     }
 
-    private void Awake()
-    {
-        DontDestroyOnLoad(gameObject);
-    }
+
+    //public static string activeCharacter;
+    //public static GameObject character;
+
+    //private void Awake()
+    //{
+    //    DontDestroyOnLoad(gameObject);
+    //}
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    //private void Update()
+    //{
+    //    character = GameObject.Find(activeCharacter);
+    //    //print("------------------ " + character.name.ToString());
+
+    //    if (character != null)
+    //    {
+    //        if (character.activeSelf)
+    //        {
+    //            print(character.activeSelf);
+    //            return;
+    //        }
+    //        else
+    //        {
+    //            character.SetActive(true);
+    //        }
+    //    }
+    //    else
+    //    {
+    //        print("character is null");
+    //    }
+    //}
 }
--- Assets/Scripts/UI/SelectionCharacter.cs
+++ Assets/Scripts/UI/SelectionCharacter.cs
@@ -1,21 +1,69 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.IO;
 
 public class SelectionCharacter : MonoBehaviour
 {
     public string nameCharacter;
 
-    // Start is called before the first frame update
     void Start()
     {
 
     }
 
-    public void ActiveCharacter()
-    {
-        CharacterManager.activeCharacter = nameCharacter;
-        SceneManager.LoadScene("MainMenu");
+    //public void ActiveCharacter()
+    //{
+    //    CharacterManager.activeCharacter = nameCharacter;
+    //    SceneManager.LoadScene("MainMenu");
 
-        print(CharacterManager.activeCharacter);
+    //    print(CharacterManager.activeCharacter);
+    //}
+
+    public static SelectionCharacter Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        //LoadCharacter();
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        //Variables that need to be stored
+        public string nameCharacter;
+    }
+
+    public void WriteCharacter()
+    {
+        SaveData data = new SaveData();
+        data.nameCharacter = nameCharacter;
+
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "/charactermanager.json", json);
+        
+        print(Application.persistentDataPath);
+       
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void LoadCharacter()
+    {
+        string path = Application.persistentDataPath + "/charactermanager.json";
+
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            nameCharacter = data.nameCharacter;
+        }
     }
 }
