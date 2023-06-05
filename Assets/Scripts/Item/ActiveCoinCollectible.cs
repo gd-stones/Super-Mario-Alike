154bebf6c6f664a1c86063ae6a2f498fe3f66f94
gd.vietnguyen@gmail.com <gd.vietnguyen@gmail.com>
2023-06-05 11:13:31 +0700

    done logic hit the tile head to bounce many coins

--- Assets/Scripts/Item/ActiveCoinCollectible.cs
+++ Assets/Scripts/Item/ActiveCoinCollectible.cs
@@ -8,13 +8,23 @@ public class ActiveCoinCollectible : MonoBehaviour
     private float playerHeadCoordinateY;
     private static bool hasSpawnedCoin = false;
 
-    private Vector3 initialPosition;
+    private Vector3 brickInitPos;
+    private Vector3 coinInitPos;
     public float moveSpeed = 5f;
+    private Quaternion coinInitRotation;
+    public float rotationSpeed = 30f;
+
+    private int coinCount = 0;
+    public int maxCoinCount = 5;
+
+    [SerializeField] private int valueCoin = 50;
 
     private void Start()
     {
         brickFootCoordinateY = transform.position.y - 0.5f;
-        initialPosition = transform.position;
+        brickInitPos = transform.position;
+        coinInitPos = coinCollectible.transform.position;
+        coinInitRotation = coinCollectible.transform.rotation;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -23,14 +33,19 @@ public class ActiveCoinCollectible : MonoBehaviour
 
         if (collision.gameObject.tag == "Player")
         {
+            if (coinCount >= maxCoinCount)
+                return;
+
             playerHeadCoordinateY = collision.gameObject.GetComponent<Transform>().position.y + 0.5f;
+            
             if (brickFootCoordinateY >= playerHeadCoordinateY && !hasSpawnedCoin)
             {
-                coinCollectible.SetActive(true);
                 hasSpawnedCoin = true;
-                StartCoroutine(ResetCoinSpawn());
+                coinCount++;
 
+                StartCoroutine(ResetCoinSpawn());
                 StartCoroutine(MoveBrick());
+                StartCoroutine(MoveCoin());
             }
         }
     }
@@ -39,22 +54,55 @@ public class ActiveCoinCollectible : MonoBehaviour
     {
         yield return new WaitForSeconds(0.3f);
         hasSpawnedCoin = false;
-        Destroy(this);
     }
 
     private IEnumerator MoveBrick()
     {
-        Vector3 targetPosition = transform.position + new Vector3(0, 0.4f, 0);
-        while (transform.position != targetPosition)
+        Vector3 targetPos = transform.position + new Vector3(0, 0.4f, 0);
+        
+        while (transform.position != targetPos)
         {
-            transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * moveSpeed);
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime * moveSpeed);
             yield return null;
         }
 
-        while (transform.position != initialPosition)
+        while (transform.position != brickInitPos)
         {
-            transform.position = Vector3.MoveTowards(transform.position, initialPosition, Time.deltaTime * moveSpeed);
+            transform.position = Vector3.MoveTowards(transform.position, brickInitPos, Time.deltaTime * moveSpeed);
             yield return null;
         }
+    }
+    
+    private IEnumerator MoveCoin()
+    {
+        coinCollectible.SetActive(true);
+        Vector3 targetPos = coinCollectible.transform.position + new Vector3(0, 2f, 0);
+        Quaternion targetRotation = Quaternion.Euler(0f, 180f, 0f);
+
+        while (coinCollectible.transform.position != targetPos)
+        {
+            coinCollectible.transform.position = Vector3.MoveTowards(coinCollectible.transform.position, targetPos, Time.deltaTime * moveSpeed);
+            coinCollectible.transform.rotation = Quaternion.RotateTowards(coinCollectible.transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            
+            yield return null;
+        }
+
+        DataManager.Instance.coin += valueCoin;
+
+        while (coinCollectible.transform.position != (targetPos - new Vector3(0, 0.3f, 0)))
+        {
+            coinCollectible.transform.position = Vector3.MoveTowards(coinCollectible.transform.position, (targetPos - new Vector3(0, 0.3f, 0)), Time.deltaTime * moveSpeed);
+            yield return null;
+        }
+
+        if (coinCount == maxCoinCount)
+        {
+            coinCollectible.SetActive(false);
+        }
+
+        coinCollectible.SetActive(false);
+
+        coinCollectible.transform.position = coinInitPos;
+        coinCollectible.transform.rotation = coinInitRotation;
     }
 }
